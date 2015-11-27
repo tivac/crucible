6a4a6306133a02bcd3a1eb747e392add27bd9e76
Pat Cavit <pcavit@gmail.com>
2015-11-26 22:19:16 -0800

    Content creation (at it's most minimal)

--- pages/content-new.js
+++ pages/content-new.js
@@ -1,13 +1,77 @@
 "use strict";
 
-var m = require("mithril");
+var m = require("mithril"),
+
+    db = require("../lib/firebase");
 
 module.exports = {
     controller : function() {
+        var ctrl    = this,
+            types   = db.child("types"),
+            entries = db.child("entries");
+        
+        ctrl.types = null;
+        ctrl.name  = m.prop("");
+        ctrl.type  = m.prop(false);
+        
+        types.on("value", function(snap) {
+            ctrl.types = snap.val();
+            ctrl.type(Object.keys(ctrl.types).shift());
+            
+            m.redraw();
+        });
+        
+        ctrl.onsubmit = function(e) {
+            var id;
 
+            e.preventDefault();
+
+            id = entries.push({
+                name : ctrl.name(),
+                type : ctrl.type(),
+                data : {}
+            });
+
+            m.route("/content/" + id.key());
+        };
     },
 
     view : function(ctrl) {
-        return m("h1", "CONTENT");
+        if(!ctrl.types) {
+            return m("h1", "Loading...");
+        }
+        
+        console.log(ctrl.name(), ctrl.type());
+        
+        return [
+            m("h1", "Add Content"),
+            m("form", { onsubmit : ctrl.onsubmit },
+                m("input[name=name]", { oninput : m.withAttr("value", ctrl.name), value : ctrl.name() }),
+                m("fieldset",
+                    m("legend", "Content type"),
+                    Object.keys(ctrl.types).map(function(type, idx) {
+                        return m("p",
+                            m("label",
+                                m("input[type=radio][name=type]", {
+                                    value    : type,
+                                    checked  : idx === 0,
+                                    onchange : m.withAttr("checked", function(checked) {
+                                        if(!checked) {
+                                            return;
+                                        }
+                                        
+                                        ctrl.type(type);
+                                    })
+                                }),
+                                ctrl.types[type].name
+                            )
+                        );
+                    })
+                ),
+                m("div",
+                    m("input[type=submit]", { value : "Add" })
+                )
+            )
+        ];
     }
 };
