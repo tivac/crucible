c7028d96933c4d93a7930396c411c04205192544
Pat Cavit <pcavit@gmail.com>
2015-12-01 16:58:46 -0800

    Slash this down until I figure out data

--- types/text-show.js
+++ types/text-show.js
@@ -6,20 +6,12 @@ var m      = require("mithril"),
     loading = require("./loading");
 
 module.exports = {
-    controller : function(options) {
-        var ctrl = this;
-        
-        ctrl.oninput = function(value) {
-            options.callback(value);
-        };
-    },
-
     view : function(ctrl, options) {
         var details = options.details;
 
         return m("label", details.name + ": ",
             m("input", assign(
-                { value : options.data || "", oninput : m.withAttr("value", ctrl.oninput) },
+                { value : options.data || "" },
                 details.attrs || {}
             ))
         );
