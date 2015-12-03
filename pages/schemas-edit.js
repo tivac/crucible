4ef7858adf78af6b9716b6c56bc5207a85289841
Pat Cavit <pcavit@gmail.com>
2015-12-03 14:56:52 -0800

    Schema editing is now just json

--- pages/schemas-edit.js
+++ pages/schemas-edit.js
@@ -2,9 +2,17 @@
 
 var m      = require("mithril"),
     assign = require("lodash.assign"),
+    Editor = require("codemirror"),
 
     types  = require("../types"),
-    db     = require("../lib/firebase");
+    db     = require("../lib/firebase"),
+
+    css    = require("./schemas-edit.css");
+
+// Require codemirror extra JS bits and bobs so it works
+require("codemirror/mode/javascript/javascript");
+require("codemirror/addon/lint/lint");
+require("../lib/cm-json-lint");
 
 module.exports = {
     controller : function() {
@@ -13,10 +21,7 @@ module.exports = {
             
             ref = db.child("schemas/" + id);
         
-        ctrl.id     = id;
-        ctrl.ref    = ref;
         ctrl.schema = null;
-        ctrl.edit   = null;
         ctrl.recent = null;
 
         ref.on("value", function(snap) {
@@ -40,28 +45,67 @@ module.exports = {
 
             m.redraw();
         });
+
+        ctrl.editorSetup = function(el, init) {
+            if(init) {
+                return;
+            }
+
+            ctrl.editor = Editor.fromTextArea(el, {
+                mode         : "application/json",
+                lint         : true,
+                gutters      : [ "CodeMirror-lint-markers" ],
+                lineNumbers  : true,
+                indentUnit   : 4,
+                lineWrapping : true
+            });
+
+            ctrl.editor.on("changes", ctrl.editorChanged);
+        };
+
+        ctrl.editorChanged = function() {
+            var config;
+
+            // Ensure JSON is still valid before applying
+            try {
+                config = JSON.parse(ctrl.editor.doc.getValue());
+            } catch(e) {
+                return;
+            }
+
+            ref.child("fields").set(config);
+        };
     },
 
     view : function(ctrl) {
         if(!ctrl.schema) {
             return m("h1", "Loading...");
         }
-        
-        return [
-            m("h1", ctrl.schema.name),
-            m("h2", "Recent Entries"),
-            m("ul",
-                Object.keys(ctrl.recent || {}).map(function(key) {
-                    return m("li",
-                        m("a", { href : "/content/" + key, config : m.route }, ctrl.recent[key].name)
-                    );
-                })
+
+        return m("div", { class : css.page.join(" ") },
+            m("div", { class : css.meta.join(" ") },
+                m("h1", ctrl.schema.name),
+                m("h2", "Recent Entries"),
+                m("ul",
+                    Object.keys(ctrl.recent || {}).map(function(key) {
+                        return m("li",
+                            m("a", { href : "/content/" + key, config : m.route }, ctrl.recent[key].name)
+                        );
+                    })
+                ),
+                m("hr")
             ),
-            m("hr"),
-            m.component(types.components.fields.edit, {
-                details : ctrl.schema,
-                ref     : ctrl.ref
-            })
-        ];
+            m("div", { class : css.contents.join(" ") },
+                m("div", { class : css.editor.join(" ") },
+                    m("textarea", { config : ctrl.editorSetup, },
+                        JSON.stringify(ctrl.schema.fields || {}, null, 4)
+                    )
+                ),
+
+                m("div", { class : css.fields.join(" ") },
+                    m.component(types.components.fields.show, { details : ctrl.schema })
+                )
+            )
+        );
     }
 };
