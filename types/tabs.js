6b1396ffd8f6d399ff52b299536a2f6d8081758d
Pat Cavit <pcavit@gmail.com>
2015-12-03 17:28:03 -0800

    Get tabs working

--- types/tabs.js
+++ types/tabs.js
@@ -12,8 +12,6 @@ module.exports = {
     controller : function(options) {
         var ctrl = this;
         
-        ctrl.tab = "tab-0";
-        
         ctrl.switchtab = function(tab, e) {
             e.preventDefault();
             
@@ -21,26 +19,23 @@ module.exports = {
         };
     },
     view : function(ctrl, options) {
-        var tabs = Object.keys(options.details.tabs || {});
-        
-        return m("div",
-            m("div", { class : css.nav.join(" ") },
-                m("ul", { class : css.list.join(" ") },
-                    tabs.map(function(key) {
-                        return m("li", { class : css[key === ctrl.tab ? "item-active" : "item"].join(" ") },
-                            m("a", {
-                                href    : "#" + key,
-                                class   : css.link.join(" "),
-                                onclick : ctrl.switchtab.bind(ctrl, key)
-                            }, key)
-                        );
-                    })
-                )
+        var tabs = Object.keys(options.details.tabs || {}),
+            tab  = ctrl.tab || tabs[0];
+
+        return m("div", { class : css.nav.join(" ") },
+            m("ul", { class : css.list.join(" ") },
+                tabs.map(function(key) {
+                    return m("li", { class : css[key === tab ? "item-active" : "item"].join(" ") },
+                        m("a", {
+                            href    : "#" + key,
+                            class   : css.link.join(" "),
+                            onclick : ctrl.switchtab.bind(ctrl, key)
+                        }, key)
+                    );
+                })
             ),
             tabs.map(function(key) {
-                console.log(key, options.details.tabs[key]);
-                
-                return m("div", { class : css[key === ctrl.tab ? "contents-active" : "contents"].join(" ") },
+                return m("div", { class : css[key === tab ? "contents-active" : "contents"].join(" ") },
                     m.component(types.components.fields, {
                         ref     : options.ref && options.ref.child(key),
                         // data    : ctrl.entry,
