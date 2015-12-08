d471ee47ca411fe308778c8f8fd735962dc74bed
Pat Cavit <pcavit@gmail.com>
2015-12-07 16:34:53 -0800

    Rename "fields" to "children" because... reasons

---
+++ types/children.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var m   = require("mithril"),
+    get = require("lodash.get"),
+
+    // Bound below
+    types;
+
+module.exports = {
+    view : function(ctrl, options) {
+        var details = options.details || [];
+
+        return m("div",
+            details.map(function(field, idx) {
+                var component = types[field.type || field];
+                
+                if(!component) {
+                    return m("div",
+                        m("p", "Unknown component"),
+                        m("pre", JSON.stringify(field, null, 4))
+                    );
+                }
+
+                return m.component(component, {
+                    details : field,
+                    index   : idx,
+                    data    : get(options, "data." + idx),
+                    ref     : options.ref && options.ref.child(idx),
+                });
+            })
+        );
+    }
+};
+
+// Have to bind these down here to avoid circular binding issues
+types = {
+    tabs : require("./tabs"),
+    select : require("./select"),
+    repeating : require("./repeating"),
+    number : require("./number"),
+    fieldset : require("./fieldset"),
+    instructions : require("./instructions"),
+    text : require("./text")
+};
