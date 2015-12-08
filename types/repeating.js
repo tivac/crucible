531eedc4d37096908a2f9cac10843cf6ad51a73d
Pat Cavit <pcavit@gmail.com>
2015-12-07 17:16:06 -0800

    Start to figure out repeating field interactions

--- types/repeating.js
+++ types/repeating.js
@@ -2,19 +2,52 @@
 
 var m      = require("mithril"),
     assign = require("lodash.assign"),
+    times  = require("lodash.times"),
     
-    children = require("./children");
+    children     = require("./children"),
+    instructions = require("./instructions"),
+
+    types = require("./types.css"),
+    css   = require("./repeating.css");
 
 module.exports = {
+    controller : function(options) {
+        var ctrl = this;
+
+        ctrl.children = (options.data && options.data.length) || 0;
+
+        ctrl.add = function(e) {
+            ++ctrl.children;
+        };
+    },
+
     view : function(ctrl, options) {
-        return m("div",
-            "Repeating " + options.details.name + ":",
-            m.component(children, {
-                details : options.details.children
-                // TODO: figure out how refs/data work here
-                // ref     : options.ref && options.ref.child
-                // data    : options.data
-            })
+        var details = options.details;
+
+        return m("div", { class : types[options.index ? "field" : "first"].join(" ") },
+            details.instructions ? m.component(instructions, { details : details.instructions }) : null,
+            options.data ?
+                options.data.map(function(data, idx) {
+                    return m("div", { class : css.child.join(" ") },
+                        m("p", idx),
+                        m.component(children, {
+                            details : details.children,
+                            data    : data,
+                            ref     : options.ref && options.ref.child(idx)
+                        })
+                    );
+                }) : [
+                    times(ctrl.children, function(idx) {
+                        return m("div", { class : css.child.join(" ") },
+                            m("p", idx),
+                            m.component(children, {
+                                details : details.children,
+                                ref     : options.ref && options.ref.child(idx)
+                            })
+                        );
+                    }),
+                    m("button", { onclick : ctrl.add }, "Add")
+                ]
         );
     }
 };
