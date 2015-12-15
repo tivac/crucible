7e6a31e37fd1275ea48e439f2ec2d6ed87c5041b
Pat Cavit <pcavit@gmail.com>
2015-12-14 16:53:38 -0800

    Broke out publishing status into its own component

---
+++ pages/content/publish-status.js
@@ -0,0 +1,66 @@
+"use strict";
+
+var m = require("mithril"),
+    moment = require("moment");
+
+module.exports = {
+    controller : function(options) {
+        var ctrl = this;
+
+        ctrl.now  = moment();
+        ctrl.date = false;
+
+        ctrl.publish = function(e) {
+            options.ref.child("_published").set(moment().subtract(10, "seconds").valueOf());
+        };
+
+        ctrl.publishAt = function(e) {
+            if(!ctrl.date || !ctrl.date.isValid()) {
+                return console.error("Invalid date"); // TODO: Really Handle
+            }
+
+            options.ref.child("_published").set(ctrl.date.valueOf());
+        };
+
+        ctrl.unpublish = function(e) {
+            options.ref.child("_published").remove();
+        };
+    },
+
+    view : function(ctrl, options) {
+        var published = options.data._published && moment(options.data._published);
+
+        if(published) {
+            return m("div",
+                m("span",
+                    published.isBefore(ctrl.now) ?
+                        "Published at " + published.format("lll") :
+                        "Publishing at " + published.format("lll")
+                ),
+                m("button", {
+                    onclick : ctrl.unpublish
+                }, "Unpublish")
+            );
+        }
+        
+        return m("div",
+            m("button", {
+                onclick : ctrl.publish
+            }, "Publish Now"),
+            " | ",
+            m("label",
+                "Publish at: ",
+                m("input", {
+                    type    : "datetime-local",
+                    value   : (ctrl.date || ctrl.now).format("YYYY-MM-DD[T]HH:mm"),
+                    oninput : m.withAttr("value", function(value) {
+                        ctrl.date = moment(value, "YYYY-MM-DD[T]HH:mm");
+                    })
+                })
+            ),
+            m("button", {
+                onclick : ctrl.publishAt
+            }, "Publish")
+        );
+    }
+};
