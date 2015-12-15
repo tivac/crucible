9704fea2ac21ebac883acc44ee020689ca04f1d3
Pat Cavit <pcavit@gmail.com>
2015-12-15 14:54:50 -0800

    Tidy up a bit

--- routes.js
+++ routes.js
@@ -4,12 +4,13 @@ var m = require("mithril");
 
 exports.default = function() {
     m.route(document.body, "/", {
-        "/"            : require("./pages/home"),
+        "/" : require("./pages/home"),
+
         "/schemas"     : require("./pages/schemas"),
         "/schemas/new" : require("./pages/schemas-new"),
         "/schemas/:id" : require("./pages/schemas-edit"),
-        "/content"     : require("./pages/content"),
-        
+
+        "/content"             : require("./pages/content"),
         "/content/:schema/:id" : require("./pages/content-edit")
     });
 };
