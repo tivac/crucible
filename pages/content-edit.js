61e64bb2e4d224d54b28c80cb5eac76f760029cb
Pat Cavit <pcavit@gmail.com>
2015-12-14 16:51:04 -0800

    First-pass at published status & scheduling
    Need to break it out into its own component.

--- pages/content-edit.js
+++ pages/content-edit.js
@@ -1,10 +1,75 @@
 "use strict";
 
 var m = require("mithril"),
-    
+    moment = require("moment"),
+
     children = require("../types/children"),
     db       = require("../lib/firebase"),
-    update   = require("../lib/update");
+    update   = require("../lib/update"),
+
+    publish;
+
+publish = {
+    controller : function(options) {
+        var ctrl = this;
+
+        ctrl.now  = moment();
+        ctrl.date = false;
+
+        ctrl.publish = function(e) {
+            options.ref.child("_published").set(moment().subtract(10, "seconds").valueOf());
+        };
+
+        ctrl.publishAt = function(e) {
+            if(!ctrl.date || !ctrl.date.isValid()) {
+                return console.error("Invalid date"); // TODO: Really Handle
+            }
+
+            options.ref.child("_published").set(ctrl.date.valueOf());
+        };
+
+        ctrl.unpublish = function(e) {
+            options.ref.child("_published").remove();
+        };
+    },
+
+    view : function(ctrl, options) {
+        var published = options.data._published && moment(options.data._published);
+
+        if(published) {
+            return m("div",
+                m("span",
+                    published.isBefore(ctrl.now) ?
+                        "Published at " + published.format("lll") :
+                        "Publishing at " + published.format("lll")
+                ),
+                m("button", {
+                    onclick : ctrl.unpublish
+                }, "Unpublish")
+            );
+        }
+        
+        return m("div",
+            m("button", {
+                onclick : ctrl.publish
+            }, "Publish Now"),
+            " | ",
+            m("label",
+                "Publish at: ",
+                m("input", {
+                    type    : "datetime-local",
+                    value   : (ctrl.date || ctrl.now).format("YYYY-MM-DD[T]HH:mm"),
+                    oninput : m.withAttr("value", function(value) {
+                        ctrl.date = moment(value, "YYYY-MM-DD[T]HH:mm");
+                    })
+                })
+            ),
+            m("button", {
+                onclick : ctrl.publishAt
+            }, "Publish")
+        );
+    }
+};
 
 module.exports = {
     controller : function() {
@@ -30,28 +95,21 @@ module.exports = {
             
             m.redraw();
         });
-
+        
         schema.on("value", function(snap) {
             ctrl.schema = snap.val();
 
             m.redraw();
-        })
-        
-        ctrl.namechange = function(name) {
-            ctrl.entry.name = name;
-        };
-        
-        ctrl.fieldchange = function(key, value) {
-            ctrl.entry.data[key] = value;
-        };
-        
-        ctrl.onsubmit = function(e) {
-            e.preventDefault();
-            
-            ctrl.entry.updated = db.TIMESTAMP;
-            
-            entry.update(ctrl.entry);
-        };
+        });
+
+        // Ensure the updated timestamp is always accurate-ish
+        entry.on("child_changed", function(snap) {
+            if(snap.key() === "updated") {
+                return;
+            }
+
+            entry.child("updated").set(db.TIMESTAMP);
+        });
     },
 
     view : function(ctrl) {
@@ -68,6 +126,10 @@ module.exports = {
                     m("a", { href : "/schemas/" + ctrl.entry._schema, config : m.route }, ctrl.schema.name)
                 )
             ),
+            m.component(publish, {
+                ref  : ctrl.ref,
+                data : ctrl.entry
+            }),
             m("hr"),
             m("div",
                 m("label",
@@ -91,6 +153,11 @@ module.exports = {
                 ref     : ctrl.ref,
                 data    : ctrl.entry,
                 root    : ctrl.ref
+            }),
+            m("hr"),
+            m.component(publish, {
+                ref  : ctrl.ref,
+                data : ctrl.entry
             })
         ];
     }
