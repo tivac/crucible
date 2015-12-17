c5d814014ec324d375d8c66d7b526c6da239cf2b
Pat Cavit <pcavit@gmail.com>
2015-12-17 11:50:43 -0800

    Simple saving of content versions

---
+++ src/pages/content/versioning.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var m  = require("mithril"),
+    
+    db = require("../../lib/firebase");
+
+module.exports = {
+    controller : function(options) {
+        var ctrl = this;
+
+        ctrl.snapshot = function(e) {
+            options.ref.once("value", function(snap) {
+                var data = snap.exportVal(),
+                    rev  = data._revision || 1,
+                    dest = db.child("versions").child(snap.key()).child(rev);
+
+                dest.set(data);
+
+                options.ref.child("_version").set(rev + 1);
+            });
+        };
+    },
+
+    view : function(ctrl, options) {
+        return m("div", { class : options.class },
+            m("span",
+                "Current Version: " + (options.data._version || 1)
+            ),
+            m("button", {
+                onclick : ctrl.snapshot
+            }, "Save new Version")
+        );
+    }
+};
