78139bfdcd5f17cd266f8f7eb58ae7edb5882997
Pat Cavit <pcavit@gmail.com>
2015-12-16 17:07:41 -0800

    Make <textarea> auto-expand to 600px tall

--- types/textarea.js
+++ types/textarea.js
@@ -5,13 +5,25 @@ var m      = require("mithril"),
     slug   = require("unique-slug"),
 
     update = require("../lib/update"),
-    types  = require("./types.css");
+    types  = require("./types.css"),
+    css    = require("./textarea.css");
 
 module.exports = {
     controller : function(options) {
         var ctrl = this;
     
-        ctrl.id = slug(options.details.name);
+        ctrl.id   = slug(options.details.name);
+        ctrl.text = options.data || "";
+
+        ctrl.resize = function(value, e) {
+            var lines;
+
+            if(options.ref) {
+                update(options.ref, null, value);
+            }
+
+            ctrl.text = value;
+        };
     },
 
     view : function(ctrl, options) {
@@ -27,17 +39,20 @@ module.exports = {
                 for   : ctrl.id,
                 class : types[details.required ? "required" : "label"]
             }, name),
-            m("textarea", assign({
-                    // attrs
-                    id       : ctrl.id,
-                    class    : types.input,
-                    required : details.required ? "required" : null,
-                    
-                    // events
-                    oninput : options.ref && m.withAttr("value", update.bind(null, options.ref, null))
-                },
-                details.attrs || {}
-            ), options.data || "")
+            m("div", { class : css.expander },
+                m("pre", { class : css.shadow }, m("span", ctrl.text), m("br")),
+                m("textarea", assign({
+                        // attrs
+                        id       : ctrl.id,
+                        class    : css.textarea,
+                        required : details.required ? "required" : null,
+
+                        // events
+                        oninput : m.withAttr("value", ctrl.resize)
+                    },
+                    details.attrs || {}
+                ), options.data || "")
+            )
         );
     }
 };
