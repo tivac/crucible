b703d5eebbd863b6ecc6b3e3c23ab58a11b3df28
Pat Cavit <pcavit@gmail.com>
2015-12-23 13:46:43 -0800

    Listings back on content page as a component

--- src/pages/content.js
+++ src/pages/content.js
@@ -4,90 +4,33 @@ var m = require("mithril"),
     
     db = require("../lib/firebase"),
 
-    nav = require("./nav"),
-
-    size = 20;
+    nav      = require("./nav"),
+    listings = require("./content/listings");
 
 module.exports = {
     controller : function() {
         var ctrl = this;
         
-        ctrl.content = {};
-        ctrl.offset  = 0;
+        ctrl.schemas = [];
 
         db.child("schemas").on("value", function(snap) {
-            ctrl.schemas = snap.val() || {};
-
             snap.forEach(function(schema) {
-                var key = schema.key();
-
-                db.child("content/" + key).startAt(ctrl.offset).endAt(size).once("value", function(content) {
-                    ctrl.content[key] = [];
-
-                    content.forEach(function(record) {
-                        var data = record.val();
-
-                        data.key = record.key();
-
-                        ctrl.content[key].push(data);
-                    });
-
-                    m.redraw();
+                ctrl.schemas.push({
+                    key  : schema.key(),
+                    name : schema.val().name
                 });
             });
 
             m.redraw();
         });
-
-        ctrl.add = function(schema) {
-            var result = db.child("content/" + schema).push({
-                    _name    : "New " + ctrl.schemas[schema].name,
-                    _created : db.TIMESTAMP
-                });
-
-            m.route("/content/" + schema + "/" + result.key());
-        };
-
-        ctrl.data = function() {
-
-        };
     },
 
     view : function(ctrl) {
         return m("div",
             m.component(nav),
             m("h1", "CONTENT"),
-            Object.keys(ctrl.schemas || {}).map(function(schemaKey) {
-                var schema = ctrl.schemas[schemaKey];
-
-                return m("div",
-                    m("h2", schema.name),
-                    m("p",
-                        m("button", { onclick : ctrl.add.bind(ctrl, schemaKey) }, "Add " + schema.name)
-                    ),
-                    m("table",
-                        m("tr",
-                            m("th", "Name"),
-                            m("th", "Created"),
-                            m("th", "Updated"),
-                            m("th", m.trust("&nbsp;"))
-                        ),
-                        ctrl.content[schemaKey] ?
-                            ctrl.content[schemaKey].map(function(data) {
-                                return m("tr", { key : data.key },
-                                    m("td",
-                                        m("a", { href : "/content/" + schemaKey + "/" + data.key, config : m.route }, data._name)
-                                    ),
-                                    m("td", data._created),
-                                    m("td", data._updated),
-                                    m("td",
-                                        m("button", "Delete")
-                                    )
-                                );
-                            }) :
-                            null
-                    )
-                );
+            ctrl.schemas.map(function(schema) {
+                return m.component(listings, { schema : schema });
             })
         );
     }
