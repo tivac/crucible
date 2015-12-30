1f7db359deaaf4ae416b59d2da91721585265168
Pat Cavit <pcavit@gmail.com>
2015-12-30 14:15:38 -0800

    npm start does all the things

--- build/serve.js
+++ build/serve.js
@@ -1,14 +1,69 @@
 "use strict";
 
-var path = require("path"),
+var fs   = require("fs"),
+    path = require("path"),
+    
+    browserify = require("browserify"),
+    duration   = require("humanize-duration"),
     
     server   = require("connect")(),
     ecstatic = require("ecstatic")(process.cwd(), {
         cache       : 0,
         handleError : false
-    });
+    }),
+    builder  = browserify("src/index.js", {
+        debug : true
+    }),
+    
+    bundling, bytes, time, done;
 
+function bundle() {
+    bundling = true;
+    
+    builder.bundle(function(err, out) {
+        if(err) {
+            return console.error(err.toString());
+        }
+        
+        fs.writeFileSync("gen/index.js", out);
+        
+        console.error(bytes.toString(), "bytes written to gen/index.js in", duration(time));
+        
+        bundling = false;
+        done && done();
+    });
+}
+
+builder.plugin("watchify");
+builder.plugin("modular-css", {
+    css : "gen/index.css"
+});
+
+// Start up watchify
+builder.on("update", bundle);
+builder.on("bytes", function(b) {
+    bytes = b;
+});
+builder.on("time", function(t) {
+    time = t;
+});
+bundle();
+
+// Log HTTP requests
 server.use(require("morgan")("dev"));
+
+// Delay responding to generated file requests until it's done
+server.use("/gen/index.js", function(req, res, next) {
+    if(!bundling) {
+        return next();
+    }
+    
+    console.log("Waiting for bundle to finish...");
+    
+    done = next;
+});
+
+// Set up basic ecstatic serving of files
 server.use(ecstatic);
 
 // SPA support
