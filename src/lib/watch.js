9e154fe1c583c4b84743f19dc53d28fd11cad2c6
Pat Cavit <pcavit@gmail.com>
2016-01-02 23:35:29 -0800

    Fix broken watching

--- src/lib/watch.js
+++ src/lib/watch.js
@@ -1,12 +1,14 @@
 "use strict";
 
+var db = require("./firebase");
+
 // Ensure the updated timestamp is always accurate-ish
 module.exports = function(ref) {
     ref.on("child_changed", function(snap) {
-        if(snap.key() === "_updated") {
+        if(snap.key() === "updated") {
             return;
         }
         
-        ref.child("_updated").set(db.TIMESTAMP);
+        ref.child("updated").set(db.TIMESTAMP);
     });
 };
