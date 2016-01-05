0556b7ee4e3e538ce1d02ad0e46c950b27e60dfb
Pat Cavit <pcavit@gmail.com>
2016-01-05 15:31:03 -0800

    Give content some more responsibility

--- src/pages/content.js
+++ src/pages/content.js
@@ -5,7 +5,8 @@ var m = require("mithril"),
     db = require("../lib/firebase"),
     
     layout   = require("./layout"),
-    listings = require("./content/listings");
+    listings = require("./content/listings"),
+    css      = require("./content.css");
 
 module.exports = {
     controller : function() {
@@ -20,8 +21,16 @@ module.exports = {
             m.redraw();
         });
         
-        ctrl.edit = function() {
-            m.route("/schema/" + ctrl.schema.key);
+        // Event handlers
+        ctrl.add = function() {
+            var result;
+                
+            result = db.child("content/" + ctrl.schema.key).push({
+                name    : "New " + ctrl.schema.name,
+                created : db.TIMESTAMP
+            });
+
+            m.route("/content/" + ctrl.schema.key + "/" + result.key());
         };
     },
 
@@ -32,7 +41,29 @@ module.exports = {
         
         return m.component(layout, {
             title   : ctrl.schema.name,
-            content : m.component(listings, { schema : ctrl.schema })
+            content : [
+                m("div", { class : css.metas },
+                    m("div", { class : css.addMeta },
+                        m("button", {
+                                onclick : ctrl.add,
+                                class   : css.add
+                            },
+                            "Add " + ctrl.schema.name
+                        )
+                    ),
+                    m("div", { class : css.editMeta },
+                        m("a", {
+                                class : css.edit,
+                                
+                                href   : m.route() + "/edit",
+                                config : m.route
+                            },
+                            "Edit Content Type"
+                        )
+                    )
+                ),
+                m.component(listings, { schema : ctrl.schema })
+            ]
         });
     }
 };
