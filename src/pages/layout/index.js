6b10c19de6cdfcc391c9451fb4c2b53817c487cd
Kriss Watt <kriss.watt@gmail.com>
2016-01-08 14:39:53 +0000

    Added a logout button to the header

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,46 +1,46 @@
 "use strict";
 
 var m = require("mithril"),
-    
+
     db = require("../../lib/firebase"),
-    
+
     css = require("./layout.css");
 
 module.exports = {
     controller : function() {
         var ctrl = this;
-        
+
         ctrl.schemas = null;
         ctrl.auth    = db.getAuth();
-        
+
         ctrl.add = function() {
             m.route("/schema/new");
         };
-        
+
         db.child("schemas").on("value", function(snap) {
             ctrl.schemas = [];
-            
+
             snap.forEach(function(schema) {
                 var val = schema.val();
-                
+
                 val.key = schema.key();
-                
+
                 ctrl.schemas.push(val);
             });
-            
+
             m.redraw();
         });
     },
-    
+
     view : function(ctrl, options) {
         var route = m.route();
-        
+
         if(!options) {
             options = false;
         }
 
         document.title = options.title || "Loading...";
-        
+
         return m("div", { class : css.outer },
             m("div", { class : css.header },
                 m("h1", { class : css.heading },
@@ -50,7 +50,7 @@ module.exports = {
                     m("div", { class : css.schemas },
                         (ctrl.schemas || []).map(function(schema) {
                             var url = "/content/" + schema.key;
-                            
+
                             return m("a", {
                                 class  : css[route === url ? "active" : "schema"],
                                 href   : url,
@@ -62,7 +62,12 @@ module.exports = {
                         class  : css.add,
                         href   : "/content/new",
                         config : m.route
-                    }, "+ Add Content Type")
+                    }, "+ Add Content Type"),
+                    m("a", {
+                        class  : css.logout,
+                        href   : "/logout",
+                        config : m.route
+                    }, "Logout")
                 ] : null,
                 options.content ? null : m("div", { class : css.progress })
             ),
