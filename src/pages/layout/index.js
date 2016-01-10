7bd3fddba2a18eae655575c2d474681d0b42c9c7
Kevin Cameron <kevinacameron@hotmail.com>
2016-01-10 01:29:04 -0800

    Added some markup but layed out nicely.

--- src/pages/layout/index.js
+++ src/pages/layout/index.js
@@ -1,46 +1,46 @@
 "use strict";
 
 var m = require("mithril"),
-    
+
     db = require("../../lib/firebase"),
-    
+
     css = require("./layout.css");
 
 module.exports = {
     controller : function() {
         var ctrl = this;
-        
+
         ctrl.schemas = null;
         ctrl.auth    = db.getAuth();
-        
+
         ctrl.add = function() {
             m.route("/schema/new");
         };
-        
+
         db.child("schemas").on("value", function(snap) {
             ctrl.schemas = [];
-            
+
             snap.forEach(function(schema) {
                 var val = schema.val();
-                
+
                 val.key = schema.key();
-                
+
                 ctrl.schemas.push(val);
             });
-            
+
             m.redraw();
         });
     },
-    
+
     view : function(ctrl, options) {
         var route = m.route();
-        
+
         if(!options) {
             options = false;
         }
 
         document.title = options.title || "Loading...";
-        
+
         return m("div", { class : css.outer },
             m("header", { class : css.header },
                 m("h1", { class : css.heading },
@@ -48,28 +48,30 @@ module.exports = {
                 ),
                 options.content ? null : m("div", { class : css.progress })
             ),
-            m("nav", 
-                { class : css.nav },
-                ctrl.auth ? [
-                    m("div", { class : css.schemas },
-                        (ctrl.schemas || []).map(function(schema) {
-                            var url = "/content/" + schema.key;
-                            
-                            return m("a", {
-                                class  : css[route === url ? "active" : "schema"],
-                                href   : url,
-                                config : m.route
-                            }, schema.name);
-                        })
-                    ),
-                    m("a", {
-                        class  : css.add,
-                        href   : "/content/new",
-                        config : m.route
-                    }, "+ Add Content Type")
-                ] : null
-            ),
-            m("section", { class : css.section }, options.content ? options.content : null)
+            m("section", { class : css.section },
+                m("nav",
+                    { class : css.nav },
+                    ctrl.auth ? [
+                        m("div", { class : css.schemas },
+                            (ctrl.schemas || []).map(function(schema) {
+                                var url = "/content/" + schema.key;
+
+                                return m("a", {
+                                    class  : css[route === url ? "active" : "schema"],
+                                    href   : url,
+                                    config : m.route
+                                }, schema.name);
+                            })
+                        ),
+                        m("a", {
+                            class  : css.add,
+                            href   : "/content/new",
+                            config : m.route
+                        }, "New Schema")
+                    ] : null
+                ),
+                m("article", { class : css.article }, options.content ? options.content : null)
+            )
         );
     }
 };
