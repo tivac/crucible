5a492ba5b9f618312239e9c649dbbf9981748cf7
Kriss Watt <kriss.watt@gmail.com>
2016-01-19 10:54:34 +0000

    Removing an errant console.log

--- src/types/lib/id.js
+++ src/types/lib/id.js
@@ -1,7 +1,5 @@
 "use strict";
 
 module.exports = function(options) {
-    console.log(options.path.join("-"));
-    
     return options.path.length ? options.path.join("-") : options.details.key;
 };
