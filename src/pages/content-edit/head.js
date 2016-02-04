e9a67681ac072b52013f7b9cab8e3cc93d157a68
Pat Cavit <pcavit@gmail.com>
2016-02-04 15:59:23 -0800

    Blocking this out some more

--- src/pages/content-edit/head.js
+++ src/pages/content-edit/head.js
@@ -10,6 +10,13 @@ module.exports = {
         var ctrl = this,
             ref  = options.ref;
         
+        ctrl.schedule   = false;
+        ctrl.unschedule = false;
+        
+        ctrl.toggle = function(force) {
+            ctrl.schedule = typeof force !== "undefined" ? Boolean(force) : !ctrl.schedule;
+        };
+        
         ctrl.publish = function() {
             ref.child("published").set(moment().subtract(10, "seconds").valueOf());
         };
@@ -48,33 +55,101 @@ module.exports = {
         }
         
         return m("div", { class : css.head },
-            m("p", { class : css.status },
-                status
-            ),
-            m("button", {
-                    class   : css.save,
-                    onclick : ctrl.save,
-                    title   : "Save your changes"
-                },
-                m("svg", { class : css.icon },
-                    m("use", { href : "/src/icons.svg#save" })
+            !ctrl.schedule ? [
+                m("p", { class : css.status },
+                    status
                 ),
-                "Save"
-            ),
-            m("div", { class : css.publishing },
-                m("button", { class : css.schedule },
+                m("div", { class : css.actions },
+                    m("button", {
+                            // Attrs
+                            class : css.save,
+                            title : "Save your changes",
+                            
+                            // Events
+                            onclick : ctrl.save
+                        },
+                        m("svg", { class : css.icon },
+                            m("use", { href : "/src/icons.svg#save" })
+                        ),
+                        "Save"
+                    )
+                ),
+                m("div", { class : css.publishing },
+                    m("button", {
+                            // Attrs
+                            class : css.schedule,
+                            title : "Schedule a publish",
+                            
+                            // Events
+                            onclick : ctrl.toggle.bind(null, undefined)
+                        },
+                        m("svg", { class : css.icon },
+                            m("use", { href : "/src/icons.svg#calendar" })
+                        ),
+                        "Schedule"
+                    ),
+                    m("button", {
+                            // Attrs
+                            class : css.publish,
+                            title : "Publish now",
+                            
+                            // Events
+                            onclick : ctrl.publish
+                        },
+                        m("svg", { class : css.icon },
+                            m("use", { href : "/src/icons.svg#calendar" })
+                        ),
+                        "Publish"
+                    )
+                )
+            ] : [
+                m("button", {
+                        // Attrs
+                        class : css.back,
+                        title : "Go back",
+                        
+                        // Events
+                        onclick : ctrl.toggle.bind(null, undefined)
+                    },
+                    m("svg", { class : css.icon },
+                        m("use", { href : "/src/icons.svg#arrow" })
+                    ),
+                    "Back"
+                ),
+                m("label", { for : "publish_at" }, "Publish at"),
+                m("input", {
+                    type : "datetime-local",
+                    id   : "publish_at"
+                }),
+                m("label", { for : "publish_until" }, "until"),
+                (!ctrl.unschedule ?
+                    m("a", {
+                        href : "#unpublish",
+                        
+                        onclick : function(e) {
+                            e.preventDefault();
+                            
+                            ctrl.unschedule = true;
+                        }
+                    }, "Forever") :
+                    m("input", {
+                        type : "datetime-local",
+                        id   : "publish_until"
+                    })),
+                 m("button", {
+                        // Attrs
+                        class : css.publish,
+                        title : "Schedule publish"
+                        
+                        // Events
+                        // onclick : ctrl.publish
+                    },
                     m("svg", { class : css.icon },
                         m("use", { href : "/src/icons.svg#calendar" })
                     ),
                     "Schedule"
-                ),
-                m("button", { class : css.publish },
-                    m("svg", { class : css.icon },
-                        m("use", { href : "/src/icons.svg#calendar" })
-                    ),
-                    "Publish"
                 )
-            )
+            ]
         );
     }
 };
