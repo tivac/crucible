bfdf39fdea1294a067f0338602d12f42af95a753
Pat Cavit <pcavit@gmail.com>
2016-02-19 12:07:07 -0800

    Clean up repeating fields some

--- src/types/repeating.js
+++ src/types/repeating.js
@@ -8,25 +8,29 @@ var m      = require("mithril"),
 
     children     = require("./children"),
 
-    css   = require("./repeating.css");
+    css   = require("./repeating.css"),
+    
+    icons = global.crucible.icons;
 
 function child(ctrl, options, data, idx) {
     return m("div", { class : css[idx === 0 ? "first" : "child"] },
-        m("div", { class : css.counter }, idx + 1),
+        m("div", { class : css.meta },
+            m("p", { class : css.counter }, idx + 1),
+            m("button", {
+                    class   : css.remove,
+                    onclick : ctrl.remove.bind(ctrl, options, data, idx)
+                },
+                m("svg", { class : css.icon },
+                    m("use", { href : icons + "#remove" })
+                )
+            )
+        ),
         m.component(children, assign({}, options, {
             fields : options.field.children,
             class  : css.fields,
             data   : data,
             path   : options.path.concat(idx)
-        })),
-        m("div", { class : css.counter },
-            m("button", {
-                    class   : css.remove,
-                    onclick : ctrl.remove.bind(ctrl, options, data, idx)
-                },
-                "Remove"
-            )
-        )
+        }))
     );
 }
 
@@ -50,26 +54,34 @@ module.exports = {
         };
 
         ctrl.remove = function(opts, data, idx) {
-            opts.data.splice(idx, 1);
-
-            ctrl.children = opts.data.length;
+            if(Array.isArray(opts.data)) {
+                opts.data.splice(idx, 1);
+                
+                ctrl.children = opts.data.length;
+            } else {
+                --ctrl.children;
+            }
             
             opts.update(opts.path, opts.data);
         };
     },
 
     view : function(ctrl, options) {
-        var field  = options.field,
-            hidden = hide(options);
+        var field   = options.field,
+            hidden  = hide(options),
+            content = ctrl.children || options.data;
         
         if(hidden) {
             return hidden;
         }
         
         return m("div", { class : options.class + " " + css.container },
-            options.data ?
-                options.data.map(child.bind(null, ctrl, options)) :
-                times(ctrl.children, child.bind(null, ctrl, options, false)),
+            // TODO: AHHHHHHHHHHHHHH
+            ctrl.children || options.data ?
+                options.data ? 
+                    options.data.map(child.bind(null, ctrl, options)) :
+                    times(ctrl.children, child.bind(null, ctrl, options, false))
+                : "No entries",
             m("button", {
                 class   : css.add,
                 onclick : ctrl.add.bind(ctrl, options)
