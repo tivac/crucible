e88492bfeea896f7672df3dd97ba9fb6b1e71003
Pat Cavit <pcavit@gmail.com>
2016-04-19 23:17:04 -0700

    Re-format slightly

--- src/routes/setup.js
+++ src/routes/setup.js
@@ -4,9 +4,13 @@ import keys from "lodash.mapkeys";
 import prefix from "../lib/prefix";
 
 export default function() {
-    m.route(document.body, prefix("/setup"), keys({
-        "/setup" : require("../pages/setup")
-    }, function(value, key) {
-        return prefix(key);
-    }));
+    m.route(
+        document.body,
+        prefix("/setup"),
+        keys({
+            "/setup" : require("../pages/setup")
+        }, function(value, key) {
+            return prefix(key);
+        })
+    );
 }
