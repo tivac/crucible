a4157226926ccdb3705e02b658a0c315efffbc29
Pat Cavit <pcavit@gmail.com>
2016-04-19 23:17:27 -0700

    Stop yelling at me, rollup

--- src/config.js
+++ src/config.js
@@ -1,8 +1,10 @@
 import url from "url";
+import assign from "lodash.assign";
 
-export default global.anthracite || {};
+var config = assign({}, window.anthracite || {});
 
 export var root = url.parse(document.baseURI).pathname;
 export var icons = document.baseURI + "gen/icons.svg";
 export var title = document.title;
 
+export default config;
