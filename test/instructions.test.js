042908b91de8b800d5adbd28b6167ecb6ad45888
Pat Cavit <github@patcavit.com>
2016-05-03 13:48:13 -0700

    Test for hiding + options.class

--- test/instructions.test.js
+++ test/instructions.test.js
@@ -17,11 +17,43 @@ t.test("instructions", (t) => {
     });
         
     t.test("view renders", (t) => {
-        var out = mq(instructions.view(null, {
+        var out = instructions.view(null, {
                 field : {}
+            });
+        
+        t.same(out, {
+            tag   : "div",
+            attrs : {
+                class : null
+            },
+            children : [ null, null ]
+        });
+        
+        t.end();
+    });
+    
+    t.test("view renders hidden", (t) => {
+        var out = mq(instructions.view(null, {
+                state : {},
+                field : {
+                    show : {
+                        field : "fooga"
+                    }
+                }
             }));
         
-        t.ok(out.has("div"));
+        t.ok(out.has(".hidden"));
+        
+        t.end();
+    });
+    
+    t.test("view respects options.class", (t) => {
+        var out = mq(instructions.view(null, {
+                field : {},
+                class : "fooga"
+            }));
+        
+        t.ok(out.has(".fooga"));
         
         t.end();
     });
