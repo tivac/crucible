db2a82857fc515f6b658bd5cc50125f5f7024359
Pat Cavit <github@patcavit.com>
2016-05-06 16:35:17 -0700

    Basic textarea tests

---
+++ test/textarea.js
@@ -0,0 +1,77 @@
+/* eslint no-shadow:0 */
+"use strict";
+
+var assert = require("better-assert"),
+    query  = require("mithril-query"),
+    
+    textarea = {};
+
+describe("Anthracite", () => {
+    before(() => require("./lib/rollup")("./src/types/textarea.js", textarea));
+    
+    describe("/types/textarea", function() {
+        var Controller, view;
+        
+        before(() => {
+            Controller = textarea.exports.controller;
+            view = textarea.exports.view;
+        });
+        
+        it("should exist", () => {
+            assert(typeof Controller === "function");
+            assert(typeof view === "function");
+        });
+            
+        it("should render", () => {
+            var ctrl = new Controller({
+                    path    : [],
+                    details : {
+                        key : "textarea"
+                    }
+                }),
+                
+                out = query(view(ctrl, {
+                    field : {}
+                }));
+            
+            assert(out.has("div"));
+        });
+        
+        it("should render hidden", () => {
+            var ctrl = new Controller({
+                    path    : [],
+                    details : {
+                        key : "textarea"
+                    }
+                }),
+                
+                out = query(view(ctrl, {
+                    state : {},
+                    field : {
+                        show : {
+                            field : "fooga"
+                        }
+                    }
+                }));
+            
+            assert(out.has(".hidden"));
+        });
+        
+        it("should respect options.class", () => {
+            var ctrl = new Controller({
+                    path    : [],
+                    details : {
+                        key : "textarea"
+                    }
+                }),
+                
+                out = query(view(ctrl, {
+                    field : {},
+                    class : "fooga"
+                }));
+            
+            assert(out.has(".fooga"));
+        });
+        
+    });
+});
