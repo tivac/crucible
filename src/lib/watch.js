245f51b6c496f837c5f88e018f7cc4df01adf973
Pat Cavit <github@patcavit.com>
2016-07-05 16:47:16 -0700

    Fix updated_by/updated_at

--- src/lib/watch.js
+++ src/lib/watch.js
@@ -2,23 +2,22 @@ import db from "./firebase";
 
 // Ensure the updated timestamp is always accurate-ish
 export default function(ref) {
+    var uid = firebase.auth().currentUser.uid;
+
     ref.on("child_changed", function(snap) {
-        var key = snap.key,
-            auth;
+        var key = snap.key;
         
         // Avoid looping forever
         if(key === "updated_at" || key === "updated_by") {
             return;
         }
         
-        auth = db.getAuth();
-        
         // Clean up any old updated timestamps floating around
         ref.child("updated").remove();
         
         ref.update({
             updated_at : db.TIMESTAMP,
-            updated_by : auth.uid
+            updated_by : uid
         });
     });
-};
+}
