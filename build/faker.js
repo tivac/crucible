a3d73ef8f1a526b0a7c5b57a30bcb1e4153dab7e
Kriss Watt <voodoochild@users.noreply.github.com>
2016-07-15 20:31:28 -0700

    Replace moment with various date-fns modules (#119)
    Fixes #114

--- build/faker.js
+++ build/faker.js
@@ -2,20 +2,20 @@
 
 var faker = require("faker"),
     Randexp = require("randexp"),
-    moment = require("moment"),
+    format = require("date-fns/format"),
     data = {},
     count = 100,
     x, y, repeating;
 
 for(x = 0; x < count; x++) {
     repeating = [];
-    
+
     for(y = faker.random.number({ min : 1, max : 10 }); y > 0; y--) {
         repeating.push({
-            start : moment(faker.date.future()).format("YYYY-MM-DD")
+            start : format(faker.date.future(), "YYYY-MM-DD")
         });
     }
-    
+
     data[new Randexp(/-\w{19}/).gen()] = {
         name    : faker.company.catchPhrase(),
         version : faker.random.number({ min : 1, max : 100 }),
@@ -23,11 +23,11 @@ for(x = 0; x < count; x++) {
         updated : faker.date.recent().valueOf(),
         fields  : {
             "make-your-choice" : faker.random.arrayElement([ 1, 2, 3 ]),
-            
+
             number    : faker.random.number(),
             text      : faker.lorem.sentence(),
             repeating : repeating,
-            
+
             tabs : faker.random.objectElement({
                 en : {
                     en : {
@@ -35,7 +35,7 @@ for(x = 0; x < count; x++) {
                         body  : faker.lorem.paragraph()
                     }
                 },
-                
+
                 de : {
                     de : {
                         title : "DE " + faker.company.catchPhrase(),
