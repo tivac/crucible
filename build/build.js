eb3b9cecf0b0ceb84137d2e8a54079f72a6e7944
Pat Cavit <github@patcavit.com>
2016-07-22 13:18:36 -0700

    Nicer build output

--- build/build.js
+++ build/build.js
@@ -1,14 +1,26 @@
 "use strict";
 
-var rollup = require("rollup").rollup,
+var fs = require("fs"),
+    
+    rollup   = require("rollup").rollup,
+    duration = require("humanize-duration"),
+    size     = require("filesize"),
 
-    config = require("./lib/rollup")();
+    config = require("./lib/rollup")(),
+    
+    start;
 
 require("./lib/files").dir();
 require("./lib/files").copy();
 
+start = Date.now();
+
 rollup(config).then(function(bundle) {
     return bundle.write(config);
+})
+.then(function() {
+    console.log("Bundle written to ./gen/index.js in %s", duration(Date.now() - start));
+    console.log("Bundle size: %s", size(fs.lstatSync(config.dest).size));
 })
 .catch(function(error) {
     console.error(error.stack);
