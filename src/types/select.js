45c3fb6369da82225d6211f54d6166fd2c916e01
Connor Cleary <connorc@arena.net>
2016-08-19 17:20:28 -0700

    Add a filler Please Select if a select does not define a selected option. Set value if select does define a selected option. [Fixes #123]

--- src/types/select.js
+++ src/types/select.js
@@ -4,12 +4,40 @@ import assign from "lodash.assign";
 import css from "./select.css";
 import multiple from "./lib/multiple.js";
 
+function findSelected(children) {
+    return children.find((option) => Boolean(option.selected));
+}
+
+function pleaseSelect() {
+    return {
+        attrs    : {},
+        name     : "Please select an option...",
+        value    : "",
+        selected : true
+    };
+}
+
 export default multiple({
         multiple : false
     },
 
     function(ctrl, options, children) {
-        var field = options.field;
+        var field = options.field,
+            hasSelected = findSelected(children),
+            indexOffset = 0,
+            optionObjs;
+
+        if(hasSelected) {
+            // If the schema defines a selected option, don't
+            // add a "please select" but do set the value immediately.
+            optionObjs = children;
+            ctrl.value(options, hasSelected.key, hasSelected.value);
+        } else {
+            // If we prepend a "please select" el, we need to
+            // adjust the index lookup in `onchange` via indexOffset
+            optionObjs = [ pleaseSelect() ].concat(children);
+            indexOffset = -1;
+        }
 
         return m("select", assign({
                 // attrs
@@ -18,12 +46,13 @@ export default multiple({
                 // events
                 onchange : function(e) {
                     var tgt = e.target,
-                        opt = children[tgt.selectedIndex];
+                        opt = children[tgt.selectedIndex + indexOffset];
 
                     ctrl.value(options, opt.key, opt.value);
                 }
             }, field.attrs),
-            children.map(function(option) {
+            
+            optionObjs.map(function(option) {
                 return m("option", assign({}, option.attrs, {
                         value    : option.value,
                         selected : option.selected ? "selected" : null
