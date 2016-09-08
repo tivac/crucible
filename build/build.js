6793b911ad212bf44f57366c24c09abc61a7437d
Pat Cavit <github@patcavit.com>
2016-09-08 12:07:48 -0700

    Remove remaining browserify gunk
    Also arrow-function up the build scripts because WHY NOT

--- build/build.js
+++ build/build.js
@@ -15,13 +15,10 @@ files.copy();
 
 start = Date.now();
 
-rollup(config).then(function(bundle) {
-    return bundle.write(config);
-})
-.then(function() {
-    console.log("Bundle written to ./gen/index.js in %s", duration(Date.now() - start));
-    console.log("Bundle size: %s", size(fs.lstatSync(config.dest).size));
-})
-.catch(function(error) {
-    console.error(error.stack);
-});
+rollup(config)
+    .then((bundle) => bundle.write(config))
+    .then(() => {
+        console.log("Bundle written to ./gen/index.js in %s", duration(Date.now() - start));
+        console.log("Bundle size: %s", size(fs.lstatSync(config.dest).size));
+    })
+    .catch((error) => console.error(error.stack));
