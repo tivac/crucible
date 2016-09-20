dbdefd54ada36352c907c7933f1620f4d666f678
Connor Thomas Cleary <connorcleary@gmail.com>
2016-09-20 13:59:38 -0700

    Objectify exports for children.js imports. (#141)
    * Use object-style exports instead of named exports for `types/____.js` `controller`s and `view`s.
    * Update `types/children.js` imports.

--- src/types/instructions.js
+++ src/types/instructions.js
@@ -2,11 +2,13 @@ import m from "mithril";
 
 import css from "./instructions.css";
 
-export function view(ctrl, options) {
-    var field  = options.field;
+export default {
+    view : function(ctrl, options) {
+        var field  = options.field;
 
-    return m("div", { class : options.class },
-        field.head ? m("p", { class : css.head }, field.head) : null,
-        field.body ? m("p", field.body) : null
-    );
-}
+        return m("div", { class : options.class },
+            field.head ? m("p", { class : css.head }, field.head) : null,
+            field.body ? m("p", field.body) : null
+        );
+    }
+};
