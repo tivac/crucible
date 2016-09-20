7490f7c5881c8569c7540a6e53f0c279494ca6c5
Connor Cleary <connorc@arena.net>
2016-09-20 13:11:57 -0700

    Objectify exports for children.js imports.

--- src/types/split.js
+++ src/types/split.js
@@ -6,19 +6,21 @@ import * as instructions from "./instructions";
 
 import css from "./split.css";
 
-export function view(ctrl, options) {
-    var field  = options.field;
-    
-    return m("div", { class : css.container },
-        field.instructions ? m.component(instructions, { field : field.instructions }) : null,
-        (field.children || []).map(function(section) {
-            return m("div", { class : css.section },
-                m.component(children, assign({}, options, {
-                    // Don't want to repeat any incoming class that children might've passed in
-                    class  : false,
-                    fields : section.children
-                }))
-            );
-        })
-    );
-}
+export default {
+    view : function(ctrl, options) {
+        var field  = options.field;
+        
+        return m("div", { class : css.container },
+            field.instructions ? m.component(instructions, { field : field.instructions }) : null,
+            (field.children || []).map(function(section) {
+                return m("div", { class : css.section },
+                    m.component(children, assign({}, options, {
+                        // Don't want to repeat any incoming class that children might've passed in
+                        class  : false,
+                        fields : section.children
+                    }))
+                );
+            })
+        );
+    }
+};
--- src/types/tabs.js
+++ src/types/tabs.js
@@ -4,41 +4,43 @@ import assign from "lodash.assign";
 import * as children from "./children";
 import css from "./tabs.css";
 
-export function controller() {
-    var ctrl = this;
+export default {
+    controller : function() {
+        var ctrl = this;
 
-    ctrl.tab = 0;
+        ctrl.tab = 0;
 
-    ctrl.switchtab = function(tab, e) {
-        e.preventDefault();
+        ctrl.switchtab = function(tab, e) {
+            e.preventDefault();
 
-        ctrl.tab = tab;
-    };
-}
+            ctrl.tab = tab;
+        };
+    },
 
-export function view(ctrl, options) {
-    var tabs   = options.field.children || [];
-    
-    return m("div", { class : options.class },
-        m("div", { class : css.nav },
+    view : function(ctrl, options) {
+        var tabs   = options.field.children || [];
+        
+        return m("div", { class : options.class },
+            m("div", { class : css.nav },
+                tabs.map(function(tab, idx) {
+                    return m("a", {
+                            class   : css[idx === ctrl.tab ? "activetab" : "tab"],
+                            href    : "#" + idx,
+                            onclick : ctrl.switchtab.bind(ctrl, idx)
+                        }, tab.name
+                    );
+                })
+            ),
             tabs.map(function(tab, idx) {
-                return m("a", {
-                        class   : css[idx === ctrl.tab ? "activetab" : "tab"],
-                        href    : "#" + idx,
-                        onclick : ctrl.switchtab.bind(ctrl, idx)
-                    }, tab.name
+                return m("div", { class : css[idx === ctrl.tab ? "activebody" : "body"] },
+                    m.component(children, assign({}, options, {
+                        class  : false,
+                        fields : tab.children,
+                        data   : options.data && options.data[tab.key],
+                        path   : options.path.concat(tab.key)
+                    }))
                 );
             })
-        ),
-        tabs.map(function(tab, idx) {
-            return m("div", { class : css[idx === ctrl.tab ? "activebody" : "body"] },
-                m.component(children, assign({}, options, {
-                    class  : false,
-                    fields : tab.children,
-                    data   : options.data && options.data[tab.key],
-                    path   : options.path.concat(tab.key)
-                }))
-            );
-        })
-    );
-}
+        );
+    }
+};
