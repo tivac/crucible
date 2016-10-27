af37219b3b169bb7421c06791192948a693ae584
Connor Thomas Cleary <connorcleary@gmail.com>
2016-10-26 17:02:20 -0700

    Three small fixes. (#160)
    * Fix dirty detector to fire on Title change. [Fixes #156]
    * Give schedule box a z-index so its always on top. [Fixes #155]
    * Do redraw on state change so Save button stays in sync. [Fixes #157]
    * Zindex updates.

--- src/pages/content-edit/content-state.js
+++ src/pages/content-edit/content-state.js
@@ -121,14 +121,16 @@ Content.prototype = {
     setField : function(path, val) {
         this.state.dates.updated_at = Date.now();
         this.state.user.updated_by  = this.user;
-
         this.state.meta.dirty = true;
 
-        return set(this.state, path, val);
+        set(this.state, path, val);
+        m.redraw();
     },
 
     titleChange : function(name) {
         this.state.meta.name = name;
+        this.state.meta.dirty = true;
+
         m.redraw();
     },
 
