d7ae8823891d02a40390bab40e2cca3a6d3946ce
kevinkace <kevinacameron@hotmail.com>
2017-02-03 08:33:00 -0800

    Broken

--- build/lib/icons.js
+++ build/lib/icons.js
@@ -1,60 +1,24 @@
 "use strict";
 
-var fs      = require("fs"),
-    path    = require("path"),
-    shell   = require("shelljs"),
-    globule = require("globule"),
-    Spriter = require("svg-sprite"),
+const svgSpriter = require("svg-spriter"),
 
-    dest = "./gen",
-
-    spriter;
+    glob = [
+        "./src/icons/*.svg"
+        // ,
+        // "!./src/icons.svg"
+    ];
 
 module.exports = function() {
-    var files = globule.find([
-            "./src/**/*.svg",
-            "!./src/icons.svg"
-        ]);
-
-    spriter = new Spriter({
-        dest : dest,
-        mode : {
-            symbol : {
-                inline : true,
-                dest   : dest,
-                defs   : true
-            }
+    console.log("things");
+    svgSpriter({
+        input : {
+            svg : glob
+        },
+        output : {
+            sprite : "./gen/icons.svg",
+            css    : "./gen/icons.css"
         }
     });
 
-    files.forEach(function(file) {
-        let data = fs.readFileSync(file, "utf8"),
-            filePath = path.resolve(file);
-
-        console.log(file);
-
-        spriter.add(
-            filePath,
-            null,
-            data
-        );
-    });
-
-    spriter.compile(function(err, res, data) {
-        if(err) {
-            console.log("Error: " + err);
-
-            return;
-        }
-
-        fs.writeFile(path.join(dest, "icons.svg"), res.symbol.sprite._contents, function(err) {
-            if(err) {
-                console.log("Error: " + err);
-
-                return;
-            }
-
-            console.log("Done!");
-        });
-    });
+    console.log("things2");
 };
