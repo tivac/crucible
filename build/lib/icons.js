849d7535baf8e88a7e261760c5e8fe931b25ec6f
kevinkace <kevinacameron@hotmail.com>
2017-02-03 23:59:05 -0800

    Icon build with svg-spritzer, added source icons

--- build/lib/icons.js
+++ build/lib/icons.js
@@ -1,24 +1,12 @@
 "use strict";
 
-const svgSpriter = require("svg-spriter"),
+const spriter = require("svg-spritzer"),
 
-    glob = [
-        "./src/icons/*.svg"
-        // ,
-        // "!./src/icons.svg"
-    ];
+    fsp = require("fs-promise"),
+
+    glob = "./src/icons/*.svg";
 
 module.exports = function() {
-    console.log("things");
-    svgSpriter({
-        input : {
-            svg : glob
-        },
-        output : {
-            sprite : "./gen/icons.svg",
-            css    : "./gen/icons.css"
-        }
-    });
-
-    console.log("things2");
+    spriter(glob)
+        .then(fsp.writeFile.bind(fsp, "./gen/icons.svg"));
 };
