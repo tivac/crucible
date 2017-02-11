f1d6697203407492a51701af5594f6b284f16302
kevinkace <kevinacameron@hotmail.com>
2017-02-10 19:36:05 -0800

    Watch

--- build/lib/icons.js
+++ build/lib/icons.js
@@ -6,21 +6,44 @@ var chokidar = require("chokidar"),
     svgstore = require("svgstore"),
     fs       = require("fs"),
     path     = require("path"),
+    debounce = require("lodash/debounce"),
 
     source = "./src/**/*.svg",
     dest   = "./gen/icons.svg";
 
 exports.watch = function() {
-    let icons = svgstore();
+    let icons = svgstore(),
+        debouncedWrite;
+
+    function writeStore() {
+        console.log("write file");
+
+        fs.writeFileSync(dest, icons.toString());
+
+        console.log("wrote file");
+    }
+
+    debouncedWrite = debounce(writeStore, 100, {
+        leading  : true,
+        trailing : false,
+        maxWait  : 500
+    });
 
     chokidar.watch(globule.find(source)).on("all", function(event, file) {
+        let name = path.parse(file).name;
+
         if(event !== "add" && event !== "change") {
             return;
         }
 
         file = "./" + file;
 
-        // icons = icons.add(path.parse(file).name, fs.readFileSync(file, "utf8"));
+        // remove icon if already in sprite
+        icons.element(`#${name}`).remove();
+        // add icon;
+        icons = icons.add(path.parse(file).name, fs.readFileSync(file, "utf8"));
+
+        debouncedWrite();
     });
 };
 
